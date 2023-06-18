c897c47493e52b793729f16cf6e9239500fc5ce4
KaterinaLukyanenko <118893094+KaterinaLukyanenko@users.noreply.github.com>
2023-06-18 23:21:57 +0300

    Update Calc.java

--- src/main/java/com/softserve/academy/service/Calc.java
+++ src/main/java/com/softserve/academy/service/Calc.java
@@ -3,19 +3,23 @@ package com.softserve.academy.service;
 public class Calc {
 
     public int add(int arg0, int arg1) {
-        return arg0 + arg1;
+        int result = arg0 + arg1;
+        return result;
     }
 
     public double add(double arg0, double arg1) {
-        return arg0 + arg1;
+        double result = arg0 + arg1;
+        return result;
     }
 
     public int sub(int arg0, int arg1) {
-        return arg0 - arg1;
+        int result = arg0 - arg1;
+        return result;
     }
 
     public double sub(double arg0, double arg1) {
-        return arg0 - arg1;
+        double result = arg0 - arg1;
+        return result;
     }
 
     public int mul(int arg0, int arg1) {
